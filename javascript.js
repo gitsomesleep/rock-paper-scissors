ed70e3f5710d16f59862d742558cb7efcfa20451
gitsomesleep <37907388+gitsomesleep@users.noreply.github.com>
2025-03-24 00:47:40 -0600

    feat: Implement computer choice function with adjusted comments

--- javascript.js
+++ javascript.js
@@ -3,8 +3,16 @@ console.log("Welcome to Rock-Paper-Scissors!");
 // Return random computer choice of either "rock", "paper", or "scissors"
 function getComputerChoice() {
     // Initialize a temp variable and set its value to a random number between one and three
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    // If the number is 1, return "rock"
-    // If the number is 2, return "paper"
-    // If the number is 3, return "scissors"
+    // Return option according to value of randomNumber
+    if (randomNumber === 1) {
+        return "rock";
+    }
+    if (randomNumber === 2) {
+        return "paper";
+    }
+    if (randomNumber === 3) {
+        return "scissors";
+    }
 }
