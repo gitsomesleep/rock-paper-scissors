a4659026994c15064edfe61cba0df7d8b5daa6cd
gitsomesleep <37907388+gitsomesleep@users.noreply.github.com>
2025-03-24 00:15:07 -0600

    chore: Add pseudocode for computer choice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+console.log("Welcome to Rock-Paper-Scissors!");
+
+// Return random computer choice of either "rock", "paper", or "scissors"
+function getComputerChoice() {
+    // Initialize a temp variable and set its value to a random number between one and three
+
+    // If the number is 1, return "rock"
+    // If the number is 2, return "paper"
+    // If the number is 3, return "scissors"
+}
